b974856fe6390839537781b16f599cec23aff258
zyh940305858 <940305858@qq.com>
2023-03-17 09:21:21 +0800

    提交W1-2学习笔记 未完成

---
+++ W1-2/contracts/Counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+    constructor(uint x) {
+        counter = x;
+    }
+    function count() public {
+        counter = counter + 1;
+    }
+} 
+
